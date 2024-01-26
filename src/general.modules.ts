f64d45efcd4c8fe7e65684b82c4d5551fca79bb7
jeanriffel <jeanriffel1@gmail.com>
2024-01-25 21:21:51 -0300

    feat: adding cors rules

--- src/general.modules.ts
+++ src/general.modules.ts
@@ -1,8 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppModule } from './General/app.module';
 import { LotteryModule } from './LotterySmartContract/lotery.module';
+import * as cors from 'cors';
 
 @Module({
   imports: [AppModule, LotteryModule],
 })
-export class GeneralModule {}
+export class GeneralModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(cors({ origin: 'http://localhost:3000' })).forRoutes('*');
+  }
+}
